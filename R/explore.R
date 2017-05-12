82f34657dd04d497b1033e05c532536aa8dea71d
BroVic <victorordu@outlook.com>
2017-05-12 14:05:40 +0100

    Updates documentation of explore()

--- R/explore.R
+++ R/explore.R
@@ -1,6 +1,11 @@
-#' Explore social media data
+#' Explore Social Media Data.
+#' 
+#' \code{explore} Launches a new Shiny application.
 #'
-#'
+#' This is a wrapper function for shiny::runApp, which is the core 
+#' function used to launch Shiny applications.
+#' 
+#' @return This function does not return. See \code{\link{runApp}}
 #' @export
 explore <- function()
 {
