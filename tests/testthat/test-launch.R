735c8be3cff377541582ff59db3d40b61ed3d43f
Victor Ordu <victorordu@outlook.com>
2017-07-05 12:50:24 +0100

    Initial commit

---
+++ tests/testthat/test-launch.R
@@ -0,0 +1,13 @@
+context("App launcher")
+
+test_that("Caller provides correct input", {
+  err <- "unused argument"
+  expect_error(explore(1), err)
+  expect_error(explore("a"), err)
+  expect_error(explore(T), err)
+  expect_error(explore(NA), err)
+  expect_error(explore(NULL), err)
+})
+
+test_that("Function is working against previous settings",
+          expect_equal_to_reference(object = app, file = "app-ref.rds"))
