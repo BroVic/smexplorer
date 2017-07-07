08259a62f75d627e6b24003e8e0140cb1ce60e1d
Victor Ordu <victorordu@outlook.com>
2017-07-07 16:46:02 +0100

    Deleted files

--- R/TextOnLoadedTweets.R
+++
@@ -1,5 +0,0 @@
-#' Package Objects.
-#'
-#' \code{TextOnLoadedTweets} A character vector for use on the user interface.
-textOnLoadedTweets <-
-  "tweets were downloaded. Select a value to extend the download limit: "
--- R/TextOnTweetsByPlatform.R
+++
@@ -1,4 +0,0 @@
-#' Package Objects.
-#'
-#' \code{TextOnTweetsByPlatform} A string used for labelling plots.
-textOnTweetsByPlatform <- 'Number of tweets posted by platform'
