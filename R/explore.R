417d599020e54741f55ab1a207b9e438eba87565
Victor Ordu <victorordu@outlook.com>
2017-10-03 15:49:47 +0100

    Increases parameterization of app launch

--- R/explore.R
+++ R/explore.R
@@ -1,14 +1,17 @@
 #' Explore Social Media Data.
-#' 
+#'
 #' \code{explore} Launches a new Shiny application.
 #'
-#' This is a wrapper function for shiny::runApp, which is the core 
+#' This is a wrapper function for shiny::runApp, which is the core
 #' function used to launch Shiny applications.
-#' 
+#'
 #' @return This function does not return. See \code{\link{runApp}}
 #' @export
 explore <- function()
 {
-  shiny::runApp(app)
+  shiny::runApp(app,
+                launch.browser = TRUE,
+                display.mode = "normal",
+                quiet = TRUE)
 }
 
